b360e08cfc42fab33a0b63d4299b80370fb5189e
tahbjee <noljko13@gmail.com>
2024-01-15 19:25:56 -0300

    second commit

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-function createDivs(num) {
-  for (let i = 0; i < num; i++) {
-    let container = document.querySelector(".container");
-    let newDiv = document.createElement("div");
+sizeOfGrids = 10;
 
-    newDiv.className = "createdDivs";
+let container = document.querySelector(".container");
 
+let resetButton = document.querySelector("button");
+
+function createDivs(amtOfGrids) {
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("wrapper");
+
+  for (let i = 0; i < amtOfGrids; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add = "new-divs";
+
+    for (let j = 0; j < amtOfGrids; j++) {
+      const widthAndHeight = 500 / sizeOfGrids;
+      const gridBox = document.createElement("div");
+      gridBox.classList.add("grid-box");
+      gridBox.style.width = `${widthAndHeight}px`;
+      gridBox.style.height = `${widthAndHeight}px`;
+
+      newDiv.appendChild(gridBox);
+    }
     container.appendChild(newDiv);
   }
 }
 
-let theDivs = prompt("");
+resetButton.addEventListener("click", () => {
+  let userSize = Number(prompt("Select the size"));
 
-createDivs(theDivs);
+  while (userSize > 100) {
+    userSize = Number(
+      prompt("Make sure to select a number that is equal to or less than 100. ")
+    );
+  }
+  createDivs(userSize);
+});
 
 function getRandomColor() {
   let randomR = Math.floor(Math.random() * 256);
@@ -21,10 +44,18 @@ function getRandomColor() {
   return `rgb(${randomR},${randomG},${randomB})`;
 }
 
-let elements = document.querySelectorAll(".createdDivs");
+let elements = document.querySelectorAll(".grid-box");
 
 elements.forEach(function (element) {
   element.addEventListener("mouseover", () => {
     element.style.backgroundColor = getRandomColor();
   });
 });
+
+function getRandomColor() {
+  let randomR = Math.floor(Math.random() * 256);
+  let randomG = Math.floor(Math.random() * 256);
+  let randomB = Math.floor(Math.random() * 256);
+
+  return `rgb(${randomR},${randomG},${randomB})`;
+}
